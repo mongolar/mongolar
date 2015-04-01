5c0bcee092fadcc64c7b9abbb251bf27ae187bf6
Jason <jasonrichardsmith@gmail.com>
2015-03-31 23:42:31 -0400

    rewrite of router that should cover the basics

--- router/router.go
+++ router/router.go
@@ -2,62 +2,91 @@ package router
 
 import (
 	//"github.com/davecgh/go-spew/spew"
-	"github.com/gorilla/context"
 	"github.com/jasonrichardsmith/mongolar/configs/site"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"text/template"
 )
 
-type HostSwitch map[string]http.Handler
+const (
+	MongolarScript = `mongular.constant('mongularConfig', {
+    			mongular_url: '/{{ .APIEndPoint }}/',
+    			templates_url: '/{{ .TemplateEndpoint }}'}
+		);
+		mongular.config(function($sceDelegateProvider) {
+   			$sceDelegateProvider.resourceUrlWhitelist([
+     				// Allow same origin resource loads.
+     				'self',
+				{{ range .ForeignDomains }}
+     				'http://{{.}}/**',
+				{{ end }}
+   			]);
+ 		});`
+)
 
-//var Switch HostSwitch
-var Switch = make(HostSwitch)
-
-var Routers = make(map[string]*httprouter.Router)
-var Aliases map[string]string
-var Sites map[string]*site.SiteConfig
-var HandlersMap map[string]httprouter.Handle
-
-func Build(s map[string]*site.SiteConfig, a map[string]string, hm map[string]httprouter.Handle) HostSwitch {
-	Sites = s
-	Aliases = a
-	HandlersMap = hm
-	buildRouters()
-	buildHostSwitch()
-	return Switch
+type Router struct {
+	Aliases     map[string]string
+	Sites       map[string]*site.SiteConfig
+	Controllers map[string]*controller.Controller
+	APIEndPoint string
 }
 
-func buildRouters() {
-	for k, s := range Sites {
-		buildRouter(s, k)
-	}
+func NewRouter() {
+	//TODO: build the constructor
 }
 
-func buildRouter(s *site.SiteConfig, key string) {
-	router := httprouter.New()
-	for path, handle := range HandlersMap {
-		router.GET(path, wrapHandler(handle, s))
-	}
-	Routers[key] = router
+type MongolarJSConfig struct {
+	APIEndPoint      string
+	TemplateEndpoint string
+	ForeignDomains   []string
 }
 
-func buildHostSwitch() {
-	for domain, key := range Aliases {
-		Switch[domain] = Routers[key]
-	}
-}
+func (ro Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if val, ok := ro.Alias[r.Host]; ok {
+		urlpath = split("\\", r.URL.Path)
+		pathvalues := make([]string, len(urlpath))
+		i := 0
+		for k := range mymap {
+			pathvalues[i] = k
+			i++
+		}
+		s := ro.Sites[r.Aliases[r.Host]]
+		switch pathvalues[0] {
+		case "mongolar_config.js":
+			c := MongolarJSConfig{APIEndPoint: ro.APIEndPoint, TemplateEndpoint: s.TemplateEndpoint, ForeignDomains: s.ForeignDomains}
+			c.serveMongolarConfig(w)
+		case "assets":
+			directory := ro.Sites[r.Aliases[r.Host]].Directory
+			http.FileServer(http.Dir(directory + "/assets"))
 
-func (hs HostSwitch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if handler := hs[r.Host]; handler != nil {
-		handler.ServeHTTP(w, r)
+		case r.APIEndPoint:
+			if val, ok := ro.Controllers[pathvalues[1]]; ok {
+				content := ro.Controllers[pathvalues[1]].getContent(r, ro.Sites[r.Aliases[ro.Host]])
+			} else {
+				http.Error(w, "Forbidden", 403)
+			}
+		default:
+			directory := ro.Sites[r.Aliases[ro.Host]].Directory
+			http.ServeFile(w, r, directory+"/index.html")
+		}
+
 	} else {
-		http.Error(w, "Forbidden", 403) // Or Redirect?
+		http.Error(w, "Not Found", 404) // Or Redirect?
 	}
+
 }
 
-func wrapHandler(h httprouter.Handle, s *site.SiteConfig) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		context.Set(r, "", s)
-		h(w, r, ps)
+func (c *MongolarJSConfig) serveMongolarConfig(c MongolarJSConfig, w http.ResponseWriter) {
+	t := template.New("Mongolar Config JS")
+	t, err := t.Parse(MongolarScript)
+	err = t.Execute(w, c)
+}
+
+func rand_str(size int) string {
+	alphanum := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, size)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
 	}
+	return string(bytes)
 }
