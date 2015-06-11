4ba169fdf689a945f009d7f3ae7183b52756076f
Jason Smith <jasonrichardsmith@gmail.com>
2015-06-11 08:26:35 -0400

    added menu element

---
+++ models/elements/menu.go
@@ -0,0 +1,19 @@
+package elements
+
+type MenuItem struct {
+	Title    string      `bson:"title"`
+	Url      string      `bson:"url"`
+	Children interface{} `bson:"menu_items"`
+}
+
+type MenuElement struct {
+	MenuItems []MenuItem `bson:"controller_values" json:"menu_items"`
+	Element   `bson:"_,inline"`
+}
+
+func NewMenuElement() SlugElement {
+	e := NewElement()
+	menuitems := make([]MenuItem, 0)
+	me := MenuElement{Element: e, MenuItems: menuitems}
+	return me
+}
